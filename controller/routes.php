7326ee1b077872e28562845364533bb5e4a8bbd1
RahadyanRizqy <rahadyanrizqy@gmail.com>
2024-05-02 22:38:37 +0700

    adding chart template and route revision part 2

--- controller/routes.php
+++ controller/routes.php
@@ -3,37 +3,6 @@ include_once 'config/static.php';
 include_once 'controller/main.php';
 include_once 'function/main.php';
 
-
-class Router { 
-    public static $urls = [];
-
-    function __construct() {
-        $url = implode("/", 
-            array_filter(
-                explode("/", 
-                    str_replace($_ENV['BASEDIR'], "", 
-                        parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
-                    )
-                ), 'strlen'
-            )
-        );
-
-        if (!in_array($url, self::$urls['routes'])) {
-            header('Location: '.BASEURL);
-        }
-        
-        $call = self::$urls[$_SERVER['REQUEST_METHOD']][$url];
-        $call();
-    }
-    public static function url($url, $method, $callback) {
-        if ($url == '/') { $url = ''; }
-        self::$urls[strtoupper($method)][$url] = $callback;
-        self::$urls['routes'][] = $url;
-        self::$urls['routes'] = array_unique(self::$urls['routes']);
-    }
-
-}
-
 # GET
 Router::url('/', 'get', function () { return view('home'); });
 Router::url('login', 'get', 'AuthController::login');
