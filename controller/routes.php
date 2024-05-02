844be5a2dec9df8cc6f86a5a7803dd6d54c07936
RahadyanRizqy <rahadyanrizqy@gmail.com>
2024-05-02 22:36:46 +0700

    adding chart template and route revision

--- controller/routes.php
+++ controller/routes.php
@@ -3,7 +3,12 @@ include_once 'config/static.php';
 include_once 'controller/main.php';
 include_once 'function/main.php';
 
-$url = implode("/", 
+
+class Router { 
+    public static $urls = [];
+
+    function __construct() {
+        $url = implode("/", 
             array_filter(
                 explode("/", 
                     str_replace($_ENV['BASEDIR'], "", 
@@ -13,33 +18,39 @@ $url = implode("/",
             )
         );
 
-# GET
-route('/', 'get', function () { return view('home'); });
+        if (!in_array($url, self::$urls['routes'])) {
+            header('Location: '.BASEURL);
+        }
+        
+        $call = self::$urls[$_SERVER['REQUEST_METHOD']][$url];
+        $call();
+    }
+    public static function url($url, $method, $callback) {
+        if ($url == '/') { $url = ''; }
+        self::$urls[strtoupper($method)][$url] = $callback;
+        self::$urls['routes'][] = $url;
+        self::$urls['routes'] = array_unique(self::$urls['routes']);
+    }
 
-route('login', 'get', 'AuthController::login');
-route('register', 'get', 'AuthController::register');
-
-route('dashboard', 'get', 'DashboardController::index');
-route('dashboard/admin', 'get', 'DashboardController::admin');
-route('dashboard/contacts', 'get', 'DashboardController::contacts');
-route('dashboard/logout', 'get', 'AuthController::logout');
-
-route('contacts/add', 'get', 'ContactController::add');
-route('contacts/edit', 'get', 'ContactController::edit');
-route('contacts/remove', 'get', 'ContactController::remove');
-
-route('freshdb', 'get', 'freshdb');
-
-# POST
-route('login', 'post', 'AuthController::saveLogin');
-route('register', 'post', 'AuthController::saveRegister');
-
-route('contacts/add', 'post', 'ContactController::saveAdd');
-route('contacts/edit', 'post', 'ContactController::saveEdit');
-
-if (!in_array($url, $urls['routes'])) {
-    header('Location: '.BASEURL);
 }
 
-$call = $urls[$_SERVER['REQUEST_METHOD']][$url];
-$call();
+# GET
+Router::url('/', 'get', function () { return view('home'); });
+Router::url('login', 'get', 'AuthController::login');
+Router::url('register', 'get', 'AuthController::register');
+Router::url('dashboard', 'get', 'DashboardController::index');
+Router::url('dashboard/admin', 'get', 'DashboardController::admin');
+Router::url('dashboard/contacts', 'get', 'DashboardController::contacts');
+Router::url('dashboard/logout', 'get', 'AuthController::logout');
+Router::url('contacts/add', 'get', 'ContactController::add');
+Router::url('contacts/edit', 'get', 'ContactController::edit');
+Router::url('contacts/remove', 'get', 'ContactController::remove');
+Router::url('freshdb', 'get', 'freshdb');
+
+# POST
+Router::url('login', 'post', 'AuthController::saveLogin');
+Router::url('register', 'post', 'AuthController::saveRegister');
+Router::url('contacts/add', 'post', 'ContactController::saveAdd');
+Router::url('contacts/edit', 'post', 'ContactController::saveEdit');
+
+new Router();
--- index.php
+++ index.php
@@ -1,3 +1,2 @@
 <?php
 include_once 'controller/routes.php';
-?>
